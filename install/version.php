2cdfc0a014354962ab5aba4dbdf5e1efe92162d2
IvanShishkin <i.shishkin@intensa.ru>
2021-05-25 12:37:18 +0300

    добавил updater

--- install/version.php
+++ install/version.php
@@ -1,7 +1,7 @@
 <?
 $arModuleVersion = array(
-	"VERSION" => "2.4.0",
-	"VERSION_DATE" => "2021-05-19 11:05:00",
+	"VERSION" => "2.4.1",
+	"VERSION_DATE" => "2021-05-24 20:21:05",
     "MODULE_NAME" => GetMessage("MODULE_NAME"),
     "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
 );
--- updater.php
+++ updater.php
@@ -1,3 +1,16 @@
 <?
+if (IsModuleInstalled('mindbox.marketing')) {
 
+    $obj = CModule::CreateModuleObject('mindbox.marketing');
+    $curVersion = $obj->MODULE_VERSION;
+    $allowUpdateVersion = '2.3.0';
+
+    $objEventController = new \Mindbox\EventController();
+    $objEventController->unInstallEvents();
+    $objEventController->installEvents();
+    $objEventController->revisionHandlers();
+
+    $objHlInstaller = new \Mindbox\Installer\CartRulesInstaller();
+    $objHlInstaller->install();
+}
 
