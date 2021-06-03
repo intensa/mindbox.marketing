6a17cbcfdcd56f1a2a970b04da513d5e2b4a332e
IvanShishkin <i.shishkin@intensa.ru>
2021-06-03 14:32:17 +0300

    скрипт updater.php. Срабатывает при установке обновлений

--- updater.php
+++ updater.php
@@ -1,3 +1,21 @@
 <?
 
+if (IsModuleInstalled('mindbox.marketingdev')) {
 
+    $obj = CModule::CreateModuleObject('mindbox.marketingdev');
+    $curVersion = $obj->MODULE_VERSION;
+    $allowUpdateVersion = '2.3.0';
+
+    $objEventController = new \Mindbox\EventController();
+    $optionsEvent = \COption::SetOptionString('mindbox.marketing', 'ENABLE_EVENT_LIST', '');
+
+    if (empty($optionsEvent)) {
+        $objEventController->unInstallEvents();
+        $objEventController->installEvents();
+    }
+
+    $objEventController->revisionHandlers();
+
+    $objHlInstaller = new \Mindbox\Installer\CartRulesInstaller();
+    $objHlInstaller->install();
+}
